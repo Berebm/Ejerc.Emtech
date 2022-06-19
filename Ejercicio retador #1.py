0fd711fe8c9dddae760eb68e031c3812b7062a9a
Berebm <107776435+Berebm@users.noreply.github.com>
2022-06-18 18:04:31 -0600

    Add files via upload

---
+++ Ejercicio retador #1.py
@@ -0,0 +1,20 @@
+#Ejercicio retador 1
+superficieKm2 = 57,365 
+tempAnualGradosCentigrados = 25.45
+precipitAnualmm = 790.1 
+
+poblaFemenina = 1532128 
+poblaMasculina = 1494815
+
+porcentajeHabCuliacan = 33.15
+porcentajeHabMazatlan = 16.57
+
+clima = 'calido, subhumedo, seco y semiseco'
+
+poblacionTotal = poblaFemenina + poblaMasculina
+porcentajeTotal = porcentajeHabCuliacan + porcentajeHabMazatlan
+temp_clima = "El clima, con una precipitacion en mm de " + str(precipitAnualmm) + " " + clima
+
+print(poblacionTotal)
+print(porcentajeTotal)
+print(temp_clima)
